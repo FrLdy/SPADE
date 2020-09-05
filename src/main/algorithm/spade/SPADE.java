14ed54ba5d618c4cae040730666a1a35b6420f24
François Ledoyen <ledoyenfrancois@gmail.com>
2020-09-05 14:34:26 +0200

    Update SPADE.java
    Compute 1 and 2 sequences

--- src/main/algorithm/spade/SPADE.java
+++ src/main/algorithm/spade/SPADE.java
@@ -1,31 +1,37 @@
 package main.algorithm.spade;
 
+import main.algorithm.measure.Frequency;
 import main.algorithm.measure.IMeasure;
 import main.dataset.Dataset;
+import main.algorithm.spade.deserializer.SequenceSPMFDeserializer;
+import main.dataset.IDataset;
 import main.pattern.Item;
 import main.pattern.Itemset;
 
+import javax.xml.transform.Source;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class SPADE<T extends Comparable<? super T>> {
-    float minSup;
+    Double minSup;
     public Dataset<Sequence<T>> entryDataset;
-    Dataset<Sequence<T>> outputDataset;
-    EquivalenceClass root;
-    IMeasure<Double> measure;
-    List<Sequence<T>> oneSequences = new ArrayList<>();
-    List<Sequence<T>> twoSequences = new ArrayList<>();
+    private Dataset<Sequence<T>> outputDataset;
+    private EquivalenceClass root = new EquivalenceClass();
+    private IMeasure<Double> measure;
+    private CandidateGenerator<T> candidateGenerator = new CandidateGenerator<>();
+    private List<Sequence<T>> oneSequences = new ArrayList<>();
+    private List<Sequence<T>> twoSequences = new ArrayList<>();
 
 
-    public SPADE(float minSup, Dataset<Sequence<T>> entryDataset, Dataset<Sequence<T>> outputDataset) {
+    public SPADE(Double minSup, Dataset<Sequence<T>> entryDataset, Dataset<Sequence<T>> outputDataset) {
         this.minSup = minSup;
         this.entryDataset = entryDataset;
         this.outputDataset = outputDataset;
+        this.measure = new Frequency(entryDataset.size());
     }
 
-    public SPADE(float minSup, Dataset<Sequence<T>> entryDataset) {
+    public SPADE(Double minSup, Dataset<Sequence<T>> entryDataset) {
         this(minSup, entryDataset, new Dataset<>());
     }
 
@@ -48,14 +54,85 @@ public class SPADE<T extends Comparable<? super T>> {
         for (Item<T> key : res.keySet()){
             Sequence<T> s = res.get(key);
             EquivalenceClass candidateEqC = new EquivalenceClass(s);
-            if (this.measure.computePotentialValue(candidateEqC) >= minSup){
-                this.root.addMember(new EquivalenceClass(s));
+            if (keepSeq(candidateEqC)){
+                this.root.addMember(candidateEqC);
                 this.oneSequences.add(s);
             }
         }
     }
 
-    public float getMinSup() {
+    private void computeTwoSequences(){
+        Sequence<T> s1, s2, cand;
+        EquivalenceClass candidateEqC;
+        for (int i = 0; i < oneSequences.size(); i++){
+            // <s1> <s1>;
+            s1 = oneSequences.get(i);
+            cand = candidateGenerator.temporalJoin(s1, s1);
+            candidateEqC = new EquivalenceClass(cand);
+            if (keepSeq(candidateEqC)){
+                this.root.getMembers().get(i).addMember(candidateEqC);
+                this.twoSequences.add(cand);
+            }
+
+            for (int j = i+1; j < oneSequences.size(); j++){
+                s2 = oneSequences.get(j);
+
+                // <s1, s2>
+                cand = candidateGenerator.equalityJoin(s1, s2);
+                candidateEqC = new EquivalenceClass(cand);
+                if (keepSeq(candidateEqC)){
+                    this.root.getMembers().get(i).addMember(candidateEqC);
+                    this.twoSequences.add(cand);
+                }
+
+                // <s1>, <s2>
+                cand = candidateGenerator.temporalJoin(s1, s2);
+                candidateEqC = new EquivalenceClass(cand);
+                if (keepSeq(candidateEqC)){
+                    this.root.getMembers().get(i).addMember(candidateEqC);
+                    this.twoSequences.add(cand);
+                }
+
+                // <s2>, <s2>
+                cand = candidateGenerator.temporalJoin(s2, s1);
+                candidateEqC = new EquivalenceClass(cand);
+                if (keepSeq(candidateEqC, true)){
+                    this.root.getMembers().get(j).addMember(candidateEqC);
+                    this.twoSequences.add(cand);
+                }
+            }
+        }
+    }
+
+    public Double getMinSup() {
         return minSup;
+    }
+
+    private boolean keepSeq(EquivalenceClass equivalenceClass, boolean verbose){
+        Double sup = this.measure.computePotentialValue(equivalenceClass);
+        if (verbose){
+            System.out.println(equivalenceClass.sequence + " " + sup);
+        }
+        return sup >= getMinSup();
+    }
+
+    private boolean keepSeq(EquivalenceClass equivalenceClass){
+        return this.keepSeq(equivalenceClass, false);
+    }
+
+    private void printEqCl(EquivalenceClass equivalenceClass){
+        if (!equivalenceClass.getMembers().isEmpty()){
+            for (EquivalenceClass e : equivalenceClass.getMembers()){
+                printEqCl(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Dataset<Sequence<String>> dataset = (Dataset<Sequence<String>>) new SequenceSPMFDeserializer<String>("data/spadeTest1.txt").deserialize();
+        SPADE<String> spade = new SPADE<String>(0.5, dataset);
+        spade.computeOneSequences();
+        spade.computeTwoSequences();
+        spade.printEqCl(spade.root);
     }
 }
