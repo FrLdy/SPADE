5b4231bde16fe8a18676ce8baa5cf07ec3e87bf6
François Ledoyen <ledoyenfrancois@gmail.com>
2020-09-09 13:45:28 +0200

    Update Dataset.java
    toString du dataset

--- src/main/dataset/Dataset.java
+++ src/main/dataset/Dataset.java
@@ -9,6 +9,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Dataset<P extends IPattern> extends ArrayList<P> implements IDataset<P>{
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        for (P pattern : this){
+            res.append(pattern.toString());
+            res.append("\n");
+        }
+        return res.toString();
+    }
 }
 
 
